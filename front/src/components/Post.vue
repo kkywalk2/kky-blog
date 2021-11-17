cff3817d61716750ac47dade69c31a2a1b1763b7
KIYOUNG KWON <kkywalk2@gmail.com>
2021-11-18 00:25:03 +0900

    mod : Post화면의 비율수정

--- front/src/components/Post.vue
+++ front/src/components/Post.vue
@@ -1,26 +1,32 @@
 <template>
   <section>
-      <b-field :label="postTitle">
-        <b-button tag="router-link" :to="`/editor/${this.$route.params.id}`">수정</b-button>
-        <b-button v-on:click="deletePost">삭제</b-button>
-      </b-field>
-      <viewer v-if="content!=null" :initialValue="content"/>
-    <ul>
-      <li class="list-view" v-for="item in comments" :key="item.id">
-        <label>{{item.accountName}}:{{item.content}}</label>
-      </li>
-    </ul>
-    <b-field label="Comment">
-      <b-input placeholder="Enter your Comment" v-model="comment"/>
-    </b-field>
-    <b-button label="Add Comment" v-on:click="addComment"/>
+    <div class="columns is-multiline is-desktop">
+      <div class="column is-one-quarter"/>
+      <div class="column">
+        <b-field :label="postTitle">
+          <b-button tag="router-link" :to="`/editor/${this.$route.params.id}`">수정</b-button>
+          <b-button v-on:click="deletePost">삭제</b-button>
+        </b-field>
+        <viewer v-if="content!=null" :initialValue="content"/>
+        <ul>
+          <li class="list-view" v-for="item in comments" :key="item.id">
+            <label>{{ item.accountName }}:{{ item.content }}</label>
+          </li>
+        </ul>
+        <b-field label="Comment">
+          <b-input placeholder="Enter your Comment" v-model="comment"/>
+        </b-field>
+        <b-button label="Add Comment" v-on:click="addComment"/>
+      </div>
+      <div class="column is-one-quarter"/>
+    </div>
   </section>
 </template>
 
 <script>
-import 'codemirror/lib/codemirror.css'
-import '@toast-ui/editor/dist/toastui-editor.css'
-import { Viewer } from '@toast-ui/vue-editor'
+
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import {Viewer} from '@toast-ui/vue-editor'
 
 import {getPost, addComment, deletePost, checkAuthentication} from "@/service";
 
@@ -44,10 +50,10 @@ export default {
       postTitle: '',
       content: null,
       comments: [],
-      comment : ''
+      comment: ''
     };
   },
-  created: async function() {
+  created: async function () {
     let data = (await getPost(this.$route.params.id)).data
     console.log(data)
     this.postTitle = data.title
@@ -57,32 +63,32 @@ export default {
   methods: {
     async addComment() {
       try {
-        if(localStorage.getItem("token") && (await checkAuthentication(localStorage.getItem("token")))) {
+        if (localStorage.getItem("token") && (await checkAuthentication(localStorage.getItem("token")))) {
           await addComment(localStorage.getItem("token"), this.$route.params.id, this.comment)
           this.comments = (await getPost(this.$route.params.id)).data.comments
-        } else { 
+        } else {
           alert("로그인 해주세요")
           this.$router.push({name: 'Login'})
         }
-      } catch(ex) {
+      } catch (ex) {
         alert("뭔가 잘못됨! : " + ex)
       }
     },
     async deletePost() {
       try {
-        if(localStorage.getItem("token") && (await checkAuthentication(localStorage.getItem("token")))) {
+        if (localStorage.getItem("token") && (await checkAuthentication(localStorage.getItem("token")))) {
           let data = await deletePost(localStorage.getItem("token"), this.$route.params.id)
-          if(data != null) {
+          if (data != null) {
             alert("삭제완료")
             this.$router.push({name: 'Blog'})
-          }else {
+          } else {
             alert("삭제에 실패하였습니다")
           }
-        } else { 
+        } else {
           alert("로그인 해주세요")
           this.$router.push({name: 'Login'})
         }
-      } catch(ex) {
+      } catch (ex) {
         alert("뭔가 잘못됨! : " + ex)
       }
     }
