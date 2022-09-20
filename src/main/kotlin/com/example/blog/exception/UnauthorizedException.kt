ffb743c923458dca509b280f0cbbdba5aa3f724d
kkywalk2 <kkywalk2@gmail.com>
2022-09-20 22:12:53 +0900

    refactor: UnauthorizedException kotlin으로 변경

--- src/main/kotlin/com/example/blog/exception/UnauthorizedException.kt
+++ src/main/kotlin/com/example/blog/exception/UnauthorizedException.kt
@@ -1,7 +1,7 @@
-package com.example.blog.exception;
+package com.example.blog.exception
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class UnauthorizedException extends RuntimeException { }
+class UnauthorizedException : RuntimeException()
