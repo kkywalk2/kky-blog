4fe8042cb25559bfaa4344cc7d2ba3ae3bbebab6
kkywalk2 <kkywalk2@gmail.com>
2022-10-21 15:42:04 +0900

    fix: comment 추가에 transaction 추가

--- src/main/kotlin/com/example/blog/service/CommentService.kt
+++ src/main/kotlin/com/example/blog/service/CommentService.kt
@@ -3,13 +3,18 @@ package com.example.blog.service
 import com.example.blog.dto.CommentDto
 import com.example.blog.dto.CreateCommentRequest
 import com.example.blog.repository.CommentRepository
+import com.zaxxer.hikari.HikariDataSource
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.stereotype.Service
 
 @Service
-class CommentService (
-    private val commentRepository: CommentRepository
+class CommentService(
+    private val commentRepository: CommentRepository,
+    private val hikariDataSource: HikariDataSource
 ) {
-    fun addComment(accountId: Long, accountName: String, request: CreateCommentRequest): CommentDto {
-        return commentRepository.save(accountId, accountName, request)
-    }
+    fun addComment(accountId: Long, accountName: String, request: CreateCommentRequest): CommentDto =
+        transaction(Database.connect(hikariDataSource)) {
+            commentRepository.save(accountId, accountName, request)
+        }
 }
