63aa8d28905dd3ffc63446def5e2e28901ed9194
kkywalk2 <kkywalk2@gmail.com>
2023-03-28 23:07:02 +0900

    feat: PostCard에 디자인 적용 및 hover 효과 추가

--- front-react/components/AuthorCard/index.tsx
+++ front-react/components/AuthorCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import { UnstyledA } from './style';
 
 const AuthorCard = () => {
--- front-react/components/PostCard/index.tsx
+++ front-react/components/PostCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Badge, Row } from 'react-bootstrap';
+import { Badge, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router';
+import './style.css';
 
 interface Props {
   id: string;
@@ -12,13 +13,17 @@ const PostCard = ({ id, title, createdAt }: Props) => {
   const navigate = useNavigate();
 
   return (
-    <Row onClick={() => navigate(`/post/${id}`)}>
-      <h1>{title}</h1>
-      <p>summary is not avalable yet....</p>
-      <div>
-        <Badge>Posted at {createdAt}</Badge>
-      </div>
-    </Row>
+    <Card border="secondary" style={{ margin: '10px' }} onClick={() => navigate(`/post/${id}`)}>
+      <Card.Header>
+        <h1>{title}</h1>
+      </Card.Header>
+      <Card.Body>
+        <p>summary is not avalable yet....</p>
+        <div>
+          <Badge>Posted at {createdAt}</Badge>
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
 
